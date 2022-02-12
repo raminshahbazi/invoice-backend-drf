79d25932a6e4bddb47998f5f9c8c49f4a404234b
Ramin <ramin.s.farahani@gmail.com>
2022-02-12 13:50:19 +0330

    feat: added Invoice and InvoiceItem model.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Item, Invoice, InvoiceItem
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'discount', 'manufacturing_country')
+
+
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'type', 'description')
+
+
+class InvoiceItemAdmin(admin.ModelAdmin):
+    list_display = ('invoice', 'item', 'registration_date')
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Invoice, InvoiceAdmin)
+admin.site.register(InvoiceItem, InvoiceItemAdmin)
--- core/models.py
+++ core/models.py
@@ -1,6 +1,29 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+
+
+class Invoice(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    date = models.DateField(null=False, default=datetime.now)
+
+    class YearInSchool(models.TextChoices):
+        TYPE1 = 'T1', _('Type 1')
+        TYPE2 = 'T2', _('Type 2')
+        TYPE3 = 'T3', _('Type 3')
+    type = models.CharField(
+        max_length=2,
+        choices=YearInSchool.choices,
+        default=YearInSchool.TYPE1,
+    )
+    description = models.TextField(null=True)
+    registration_date = models.DateField(null=False, default=datetime.now)
+
+    def __str__(self):
+        return f"{self.user} - {self.type} - {self.description}"
 
 
 class Item(models.Model):
@@ -12,3 +35,13 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class InvoiceItem(models.Model):
+    invoice = models.ForeignKey(to=Invoice, on_delete=models.CASCADE)
+    item = models.ForeignKey(to=Item, on_delete=models.CASCADE)
+    registration_date = models.DateField(null=False, default=datetime.now)
+
+    def __str__(self):
+        return f"{self.invoice} - {self.item} - {self.registration_date}"
+
