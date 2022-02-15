b827393add4e4de21f4b5e75f8d3648cce051752
Ramin <ramin.s.farahani@gmail.com>
2022-02-15 04:16:44 +0330

    feat: added pagination setting.

--- invoiceTest/settings.py
+++ invoiceTest/settings.py
@@ -124,7 +124,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
 }
 
 
